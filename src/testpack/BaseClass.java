0694d826da9baf0c73955644809fd6ef1a3e1d57
teamlead11 <automationlead11@gmail.com>
2019-01-24 10:42:58 +0530

    created base class

---
+++ src/testpack/BaseClass.java
@@ -0,0 +1,14 @@
+package testpack;
+
+public class BaseClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		baseClassmethod();
+	}
+
+	public static void baseClassmethod() {
+		System.out.println("this is my base class");
+	}
+
+}
