59e37fa1546bdedcf78ec661d098fb2171613251
BidishaP <bidisha.pradhan@gmail.com>
2019-01-25 11:59:40 +0530

    added test case2 & modified test case1

--- src/testpack/TestCase1.java
+++ src/testpack/TestCase1.java
@@ -5,12 +5,15 @@ public class TestCase1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		test2();
+		test4();
 	}
 
 	public static void test2(){
 		
-		
 		System.out.println("this is my test case1");
 		System.out.println("this is my test case1 new");
+	}
+	public static void test4() {
+		System.out.println("this is my test case4");
 	}
 }
---
+++ src/testpack/TestCase2.java
@@ -0,0 +1,12 @@
+package testpack;
+
+public class TestCase2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		test3();
+	}
+
+	public static void test3(){
+			System.out.println("this is my test case3");
+		
+	}}
