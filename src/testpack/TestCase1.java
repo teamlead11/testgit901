0433cd31ab93967df595578682d9f71443138579
teamlead11 <automationlead11@gmail.com>
2019-01-25 10:50:16 +0530

    modified test case1

--- src/testpack/TestCase1.java
+++ src/testpack/TestCase1.java
@@ -4,11 +4,13 @@ public class TestCase1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		test1();
+		test2();
 	}
 
-	public static void test1(){
+	public static void test2(){
+		
 		
 		System.out.println("this is my test case1");
+		System.out.println("this is my test case1 new");
 	}
 }
