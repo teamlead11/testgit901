48829ccec3cfb192078d696039abe3022015aa87
senathipathy <jonam307@gmail.com>
2019-01-28 00:11:16 +0530

    Manoj testcase

--- src/testpack/TestCase1.java
+++ src/testpack/TestCase1.java
@@ -5,11 +5,17 @@ public class TestCase1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		karty();
+		manoj();
 	}
 
 	public static void karty(){
 		
 		System.out.println("This is karty");
 		System.out.println("Modified the testcase from USER@");
+	}
+	public static void manoj() {
+		System.out.println("This is manoj");
+		System.out.println("This is my test case");
+
 	}
 }
