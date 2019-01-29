372b77c22c87e54c40918adf20c4a4479f1b20ab
teamlead11 <automationlead11@gmail.com>
2019-01-29 09:33:58 +0530

    added test case4

---
+++ src/testpack/TestCase4.java
@@ -0,0 +1,13 @@
+package testpack;
+
+public class TestCase4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		newTestCase();
+	}
+
+	public static void newTestCase() {
+		System.out.println("added some test case");
+	}
+}
