e5a1d8ac3e332ff00386c7376ce5f5ff781f1dc8
teamlead11 <automationlead11@gmail.com>
2019-01-29 10:01:51 +0530

    jaga commit

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -5,6 +5,8 @@ public class BaseClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		baseClassmethod();
+		BaseClass x = new BaseClass();
+		x.add(1, 2, 3);
 	}
 
 	public static void baseClassmethod() {
@@ -15,4 +17,7 @@ public class BaseClass {
 		System.out.println("this is my base class1");
 	}
 
+	public void add(int a, int b, int c) {
+		System.out.println(a + b + c);
+	}
 }
