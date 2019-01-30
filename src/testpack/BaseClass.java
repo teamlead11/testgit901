bad88cec914e41de168bbb640ae5efccb61f7b27
jaga2906 <jagadeeshkumarmv@hotmail.com>
2019-01-30 09:39:52 +0530

    Base class modified

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -1,22 +1,29 @@
 package testpack;
+import java.util.Arrays;
 
 public class BaseClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		baseClassmethod();
 		BaseClass b = new BaseClass();
+		b.arraySortByKarty();
 		b.add(4, 5);
 		b.add(1, 2, 3);
-
-	}
-
-	public static void baseClassmethod() {
+		b.add(4, 6, 7, 8);
+		b.sorting();
+		baseClassmethod1();
+			}
+       public static void baseClassmethod() {
 		System.out.println("this is my base class");
 	}
 
 	public static void baseClassmethod1() {
 		System.out.println("this is my base class1");
+	}
+	
+	public static void baseClassVinoth() {
+		System.out.println("this is my base class2");
 	}
 
 	public void add(int a, int b) {
@@ -26,4 +33,36 @@ public class BaseClass {
 	public void add(int a, int b, int c) {
 		System.out.println(a + b + c);
 	}
+	
+public void arraySortByKarty() {
+		
+		int[] i= {1,78,3,100,5,7,0};
+		Arrays.sort(i);
+		System.out.println(Arrays.toString(i));						
+	}
+
+
+	public void add(int a, int b, int c, int d) {
+		System.out.println(a + b + c + d);
+		
+		
+	
+		
+		
+	}
+		public void sorting() {
+			int[] i= {66,22,77,44,55,11,33};
+			Arrays.sort(i);
+			System.out.println(Arrays.toString(i));
+			}
+	
+public void arrayjaga() {
+	int[] j = {13, 7, 6, 45, 21, 9, 101, 102}; 
+        Arrays.sort(j); 
+        System.out.println(Arrays.toString(j)); 
+    
 }
+}
+
+
+
