3d28fff53f9cc1d863cb653d3b2ae96ea7d18b79
Test3011 <mohamedb3011@gmail.com>
2019-01-30 10:25:05 +0530

    Added Method By Basha

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -1,10 +1,11 @@
 package testpack;
+
 import java.util.Arrays;
 
 public class BaseClass {
 
 	public static void main(String[] args) {
-		
+
 		baseClassmethod();
 		baseClassmethod1();
 	}
@@ -17,15 +18,17 @@ public class BaseClass {
 		b.add(4, 6, 7, 8);
 		b.sorting();
 		baseClassmethod1();
-			}
-       public static void baseClassmethods() {
+		b.sortingByBasha();
+	}
+
+	public static void baseClassmethods() {
 		System.out.println("this is my base class");
 	}
 
 	public static void baseClassmethod1() {
 		System.out.println("this is my base class1");
 	}
-	
+
 	public static void baseClassVinoth() {
 		System.out.println("this is my base class2");
 	}
@@ -37,20 +40,28 @@ public class BaseClass {
 	public void add(int a, int b, int c) {
 		System.out.println(a + b + c);
 	}
-	
-public void arraySortByKarty() {
-		
-		int[] i= {1,78,3,100,5,7,0};
+
+	public void arraySortByKarty() {
+
+		int[] i = { 1, 78, 3, 100, 5, 7, 0 };
 		Arrays.sort(i);
-		System.out.println(Arrays.toString(i));						
-	}
-	public void add(int a, int b, int c, int d) {
-		System.out.println(a + b + c + d);		
-	}
-		public void sorting() {
-			int[] i= {66,22,77,44,55,11,33};
-			Arrays.sort(i);
-			System.out.println(Arrays.toString(i));
-			}
+		System.out.println(Arrays.toString(i));
 	}
 
+	public void add(int a, int b, int c, int d) {
+		System.out.println(a + b + c + d);
+	}
+
+	public void sorting() {
+		int[] i = { 66, 22, 77, 44, 55, 11, 33 };
+		Arrays.sort(i);
+		System.out.println(Arrays.toString(i));
+	}
+
+	public void sortingByBasha() {
+		int[] i = { 6, 2, 7, 4, 5, 1, 3, 9, 8 };
+		Arrays.sort(i);
+		System.out.println(Arrays.toString(i));
+	}
+
+}
